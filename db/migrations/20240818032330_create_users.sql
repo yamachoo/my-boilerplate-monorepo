4719f4dc250bb5f8c30db8fbaf6317de67689bb4
yamachoo <51016095+yamachoo@users.noreply.github.com>
2024-08-18 13:47:56 +0900

    Add configuration files for migration and lint

---
+++ db/migrations/20240818032330_create_users.sql
@@ -0,0 +1,10 @@
+-- migrate:up
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- migrate:down
+DROP TABLE IF EXISTS users;
