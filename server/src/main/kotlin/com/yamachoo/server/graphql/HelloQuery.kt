a583269ad30ee8903ab41220de7bf63b03110ea8
yamachoo <51016095+yamachoo@users.noreply.github.com>
2024-08-18 21:24:07 +0900

    Add Java Bean Validation

--- server/src/main/kotlin/com/yamachoo/server/graphql/HelloQuery.kt
+++ server/src/main/kotlin/com/yamachoo/server/graphql/HelloQuery.kt
@@ -1,11 +1,17 @@
 package com.yamachoo.server.graphql
 
 import com.expediagroup.graphql.server.operations.Query
+import jakarta.validation.constraints.Size
 import org.springframework.stereotype.Component
+import org.springframework.validation.annotation.Validated
 
 @Component
+@Validated
 class HelloQuery : Query {
-    fun hello(name: String? = "World"): String {
+    fun hello(
+        @Size(min = 1, max = 10)
+        name: String? = "World",
+    ): String {
         return "Hello, $name!"
     }
 }
