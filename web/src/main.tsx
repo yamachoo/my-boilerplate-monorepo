33fa20e61271edd49702cece58ef45f215e96521
yamachoo <51016095+yamachoo@users.noreply.github.com>
2024-09-01 09:30:09 +0900

    Format with Biome

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// biome-ignore lint/style/noNonNullAssertion: This is a root element, it should always exist
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
