429870e3de90914253f3dde4608805c6ec77c56e
yamachoo <51016095+yamachoo@users.noreply.github.com>
2024-12-24 23:18:45 +0900

    Modify codegen.ts to accommodate GraphQL schema split

--- web/codegen.ts
+++ web/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: '../server/src/main/resources/graphql/schema.graphqls',
+  schema: '../server/src/main/resources/graphql/*.graphqls',
   documents: 'src/**/*.tsx',
   ignoreNoDocuments: true,
   generates: {
